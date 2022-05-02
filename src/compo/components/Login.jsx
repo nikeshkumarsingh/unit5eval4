70df4a5e2e652a377345e33f89890604bae476cc
nikeshkumarsingh <nikeshmasai1947@gmail.com>
2022-05-02 16:23:09 +0530

    commit

--- src/compo/components/Login.jsx
+++ src/compo/components/Login.jsx
@@ -1,19 +1,17 @@
 import { useEffect ,useState} from "react";
- //import {redirect}from "react-router-dom";
+ import {Navigate}from "react-router-dom";
 export const Login = () => {
   const [data,setData]=useState([]);
    const [text,setText]=useState("");
-  //  useEffect(()=>{
-  //    let data=await fetch(" http://localhost:8080/users");
-  //    data=await data.json();
-  //    setData(data)
-  //  })
-  const check=(e)=>{
-     if(e.target.value == "admin"){
-        //  redirect("./Orders")
+   const [pass,setPass]=useState("")
+  
+  const check=()=>{
+     if(text == "admin" ){
+      
+      return <Navigate to="/orders"/>
      }
      else{
-      // redirect("./NewOrder")
+      return <Navigate to="/neworder"/>
      }
   }
 
@@ -24,18 +22,18 @@ export const Login = () => {
         type="text"
         name="username"
         placeholder="Enter Username"
-        onChange={check}
+        onChange={((e)=>setText(e.target.value))}
       />
       <input
         className="password"
         type="password"
         name="password"
         placeholder="Enter password"
-        onChange={check}
+        onChange={((e)=>setPass(e.target.value))}
       />
       {/* On this button click make network req to find user with same username and password */}
       {/* get his role, if role is `admin` take him to `/orders` page otherwise take him to `/neworder` */}
-      <button className="submit">Login</button>
+      <button className="submit" onClick={check}>Login</button>
     </div>
   );
 };
